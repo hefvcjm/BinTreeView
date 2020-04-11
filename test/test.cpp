c080c3412c39caae017a327f8a2f7118b5e17df0
hefvcjm <1020657447@qq.com>
2020-04-11 22:33:52 +0800

    Added test for bintree view.

---
+++ test/test.cpp
@@ -0,0 +1,59 @@
+/**
+ * @file test.cpp
+ * @author hefvcjm (hef1995@qq.com)
+ * @brief test bintree view
+ * @version 0.1
+ * @date 2020-04-11
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#include "bintree_view.h"
+#include <stdlib.h>
+#include <time.h> 
+#include <queue>
+
+using namespace std;
+
+struct BinTree
+{
+    int val;
+    BinTree* left;
+    BinTree* right;
+    BinTree(int x): val(x), left(NULL), right(NULL){}
+    ~BinTree(){
+        if(left){
+            delete left;
+        }
+        if(right){
+            delete right;
+        }
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    srand((unsigned)time(NULL));
+    BinTree tree(0);
+    queue<BinTree*> q;
+    q.push(&tree);
+    int n = 20;
+    for(int i = 0; i < n; i++){
+        if(!q.empty()){
+            q.front()->left = new BinTree(rand() % 200);
+            i++;
+            if(i < n){
+                q.front()->right = new BinTree(rand() % 200);
+            }
+            q.push(q.front()->left);
+            q.push(q.front()->right);
+        }
+        q.pop();
+    }
+    view_tree<BinTree>(tree);
+    return 0;
+}
+
+ 
+
+
